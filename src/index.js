77dd3b40f7c512221f758578252366ca39139f68
Chlip <bialas.maciej@gmail.com>
2021-01-15 21:13:34 +0100

    Poprawilem wyglad kodu

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ function getCity() {
     var input = document.getElementById('searchTextField');
     var autocomplete = new google.maps.places.Autocomplete(input, options);
 }
+
 google.maps.event.addDomListener(window, 'load', getCity);
 const btn = document.getElementById("searchPlaceBtn");
 btn.addEventListener("click", function(){
@@ -17,17 +18,17 @@ btn.addEventListener("click", function(){
 
 // autolokalizacja
 const localization = {location: {lat: 0, lng: 0}, formatted_address: ""}
-      const KEY = "AIzaSyDyXP9UaV5zNdPtmkW2jF8_SwCiH8-QSms"
-      let findMebtn = document.getElementById("FindMeBtn");
-      let foramttedAdressParagraph = document.getElementById("info");
-      findMebtn.addEventListener("click",getLocation);
+const KEY = "AIzaSyDyXP9UaV5zNdPtmkW2jF8_SwCiH8-QSms"
+let findMebtn = document.getElementById("FindMeBtn");
+let foramttedAdressParagraph = document.getElementById("info");
+findMebtn.addEventListener("click",getLocation);
  
-      // sprawdzam dlugosc i szerokosc geograficzna, a nastepnie wrzucam je google maps api
-      function showPosition(position) {
-        const LAT = position.coords.latitude;
-        const LNG = position.coords.longitude;
-        let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${LAT},${LNG}&key=${KEY}`;
-        fetch(url)
+// sprawdzam dlugosc i szerokosc geograficzna, a nastepnie wrzucam je google maps api
+function showPosition(position) {
+    const LAT = position.coords.latitude;
+    const LNG = position.coords.longitude;
+    let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${LAT},${LNG}&key=${KEY}`;
+    fetch(url)
         .then(response => response.json() )
         .then(data => {
           // console.log(data);
@@ -36,7 +37,7 @@ const localization = {location: {lat: 0, lng: 0}, formatted_address: ""}
         })
         .catch(err => console.warn("wystapil problem"))   
       }
-      function getLocation(){
-        navigator.geolocation ? navigator.geolocation.getCurrentPosition(showPosition) : console.log("Brak wspracia wykrywania geolokalizacji w tej przeglądarce");
-        console.log(localization);
-      }
+function getLocation(){
+    navigator.geolocation ? navigator.geolocation.getCurrentPosition(showPosition) : console.log("Brak wspracia wykrywania geolokalizacji w tej przeglądarce");
+    console.log(localization);
+}
