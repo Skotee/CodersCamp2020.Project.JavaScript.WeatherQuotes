7a002980eb9b29c9e273d8fc2de462e0d3a06ce9
Joanna <joanna.kilar.jk@gmail.com>
2021-01-17 14:39:41 +0100

    review changes

--- src/input.js
+++ src/input.js
@@ -2,20 +2,22 @@ let input = document.querySelector('input');
 let datalist = document.querySelector('datalist');
 
  fetch('city_pol.txt')
-.then (response => response.text())
-.then(text => { 
-    let citiesNames = text.split("\n");
-    let citiesNamesWithoutDuplicates = [...new Set(citiesNames)];
-    let citiesNamesUpperCase = [];
+    .then (response => response.text())
+    .then(text => { 
+        let citiesNames = text.split("\n");
+        let citiesNamesWithoutDuplicates = [...new Set(citiesNames)];
+        let citiesNamesUpperCase = [];
 
-    citiesNamesWithoutDuplicates.forEach(function(obj) {
-        citiesNamesUpperCase.push(obj.toUpperCase());
+        citiesNamesWithoutDuplicates.forEach(function(obj) {
+            citiesNamesUpperCase.push(obj.toUpperCase());
+        });
+            
+        inputAutocomplete(citiesNamesUpperCase);
+    })
+    .catch(err => {
+        console.log('Request Failed', err); 
+        alert("Autouzupełnianie miast nie działa");
     });
-        
-    inputAutocomplete(citiesNamesUpperCase);
-})
-.catch(err => console.log('Request Failed', err));
-
 
 function cleanInputOptions(node) {
     while(node.hasChildNodes()) {
@@ -34,11 +36,6 @@ function inputAutocomplete(citiesNamesUpperCase) {
                     datalist.appendChild(newOption).setAttribute('value', `${name.slice(0, -1)}`);
                 }
             })
-
-        datalist.setAttribute("id", "search");
-
-        } else {
-            datalist.setAttribute("id", "");
         }
     })
 }
