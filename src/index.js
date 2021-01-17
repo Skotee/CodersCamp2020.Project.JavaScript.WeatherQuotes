1f7bac14800260590ad24b8db1b3424e449463e6
Chlip <bialas.maciej@gmail.com>
2021-01-17 19:17:29 +0100

    guzik szukaj + interfejst automatyczego wyszukiwania zostal dodany

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const KEY = "AIzaSyDyXP9UaV5zNdPtmkW2jF8_SwCiH8-QSms"
 let findMebtn = document.getElementById("FindMeBtn");
 let foramttedAdressParagraph = document.getElementById("info");
 findMebtn.addEventListener("click",getLocation);
+let temporaryCityInfo = document.getElementById("test");
  
 // sprawdzam dlugosc i szerokosc geograficzna, a nastepnie wrzucam je google maps api
 function showPosition(position) {
@@ -15,11 +16,13 @@ function showPosition(position) {
         .then(data => {
           // console.log(data);
           Object.assign(localization, {location: data.results[0].geometry.location, formatted_address: data.results[0].formatted_address});
-          foramttedAdressParagraph.innerHTML = localization.formatted_address;  
+          temporaryCityInfo.innerHTML = localization.formatted_address;  
         })
         .catch(err => console.warn("wystapil problem"))   
       }
-function getLocation(){
-    navigator.geolocation ? navigator.geolocation.getCurrentPosition(showPosition) : console.log("Brak wspracia wykrywania geolokalizacji w tej przeglądarce");
+async function getLocation(){
+    await navigator.geolocation ? navigator.geolocation.getCurrentPosition(showPosition) : console.log("Brak wspracia wykrywania geolokalizacji w tej przeglądarce");
+    
     console.log(localization);
+    
 }
